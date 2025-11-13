c53f2a54b3aca650cc80706edf0abe7d269e8da4
Frans Heeremans <mail@fransik.nl>
2025-11-13 00:26:49 +0000

    Update

--- .oh-my-zsh/custom/custom.zsh
+++ .oh-my-zsh/custom/custom.zsh
@@ -1,9 +1,13 @@
 export EDITOR=vim
 
 if [[ $(uname -s) == "Darwin" ]]; then
-	export SSH_AUTH_SOCK=$HOME/Library/Containers/com.bitwarden.desktop/Data/.bitwarden-ssh-agent.sock
+	export SSH_AUTH_SOCK="$HOME/Library/Containers/com.bitwarden.desktop/Data/.bitwarden-ssh-agent.sock"
 	export LIMA_INSTANCE=devbox
 	export LIMA_WORKDIR=/home/frans
 else
 	umask 022
+	export XDG_CONFIG_HOME="$HOME/.config"
+	export NVM_DIR="$XDG_CONFIG_HOME/nvm"
+	export NVM_SYMLINK_CURRENT=true
+	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 fi
